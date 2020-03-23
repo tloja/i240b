b4c17442402c410557cc96a713a885d69bac5d70
tloja <42680244+tloja@users.noreply.github.com>
2020-03-22 23:14:09 -0400

    completing lab7

--- submit/lab7/exercises/2-find/find.hh
+++ submit/lab7/exercises/2-find/find.hh
@@ -9,12 +9,33 @@
 template <typename Iterator>
 void loopFn(Iterator begin, Iterator end, int arg) {
   //@TODO
+    bool found = false;
+    for (auto p = begin; p != end && found == false; p++)
+    {
+        if(*p == arg)
+        {
+            found = true;
+            std::cout << "FOUND" << std::endl;
+        }
+    }
+    if(found == false)
+        {
+        std::cout << "NOT FOUND" << std::endl;
+        }
 }
 
 /** Exactly like loopFn() but no loops should be used */
 template <typename Iterator>
 void noLoopFn(Iterator begin, Iterator end, int arg) {
   //@TODO
+    if (std::find(begin, end, arg) != end)
+        {
+            std::cout << "FOUND" << std::endl;
+        }
+    else
+        {
+            std::cout << "NOT FOUND" << std::endl;
+        }
 }
 
 #endif //ifndef OUT_HH_
